a14f3e78981334fb241944daacd650579a0fbfd0
userakf <820279897@qq.com>
2022-09-14 22:37:47 +0800

    feat:hooks

--- src/components/Fetch.vue
+++ src/components/Fetch.vue
@@ -1,12 +1,22 @@
 <template>
-    <div>
-        <ul>
-            <li v-for="it in data">{{ it }}</li>
-        </ul>
+    Load post id:
+    <button v-for="i in 5" @click="id = i">{{ i }}</button>
+
+    <div v-if="error">
+        <p>Oops! Error encountered: {{ error.message }}</p>
+        <button @click="retry">Retry</button>
     </div>
+    <div v-else-if="data">Data loaded:
+        <pre>{{ data }}</pre>
+    </div>
+    <div v-else>Loading...</div>
 </template>
 
 <script setup lang="ts">
+import { computed, ref } from 'vue';
 import { useFetch } from '../hooks/useFetch';
-const { data, error } = useFetch('http://jsonplaceholder.typicode.com/posts')
+const baseUrl = 'https://jsonplaceholder.typicode.com/todos/'
+const id = ref(1)
+const url = computed(() => baseUrl + id.value)
+const { data, error, retry } = useFetch(url)
 </script>
--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,13 +1,47 @@
-import { ref } from "vue";
+import { ComputedRef, isRef, ref, unref, watchEffect } from "vue";
 
-export function useFetch(url: string) {
+export function useFetch(url: ComputedRef<string>) {
     const data = ref(null)
-    const error = ref(null)
+    const error = ref<any>(null)
 
-    fetch(url)
-        .then((res) => res.json())
-        .then((json) => (data.value = json))
-        .catch((err) => (error.value = err))
-    
-    return { data,error }
+    async function doFetch() {
+        // 在请求之前重设状态...
+        data.value = null
+        error.value = null
+        // unref() 解包可能为 ref 的值
+        const urlValue = unref(url)
+
+        try {
+            await timeout()
+            const res = await fetch(urlValue)
+            data.value = await res.json()
+        } catch (e) {
+            error.value = e
+        }
+    }
+    if (isRef(url)) {
+        // 若输入的 URL 是一个 ref，那么启动一个响应式的请求
+        watchEffect(doFetch)
+    } else {
+        // 否则只请求一次
+        // 避免监听器的额外开销
+        doFetch()
+    }
+
+
+
+    return { data, error, retry: doFetch }
+}
+
+// artificial delay
+function timeout() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Math.random() > 0.3) {
+                resolve(null)
+            } else {
+                reject(new Error('Random Error'))
+            }
+        }, 300)
+    })
 }
